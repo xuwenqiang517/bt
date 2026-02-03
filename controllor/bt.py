15893941a5ac0a340858b80c5fb595a225aa86d9
徐文强 <xuwenqiang@shizhuang-inc.com>
2026-02-03 18:19:48 +0800

    bt

--- controllor/bt.py
+++ controllor/bt.py
@@ -1,6 +1,49 @@
+import backtrader as bt
+import os
+# Create a subclass of Strategy to define the indicators and logic
+
+class SmaCross(bt.Strategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=10,  # period for the fast moving average
+        pslow=30   # period for the slow moving average
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+
+    def next(self):
+        if not self.position:  # not in the market
+            if self.crossover > 0:  # if fast crosses slow to the upside
+                self.buy()  # enter long
+
+        elif self.crossover < 0:  # in the market & cross to the downside
+            self.close()  # close long position
 
 
+cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+
+# Create a data feed
+# data = bt.feeds.YahooFinanceData(dataname='MSFT',
+#                                  fromdate=datetime(2011, 1, 1),
+#                                  todate=datetime(2012, 12, 31))
 
 
-if __name__ == "__main__":
-    print("This is the controllor/bt.py module.")
+# 获取框架内置测试数据路径
+modpath = os.path.dirname(bt.__file__)
+data_path = os.path.join(modpath, 'datas/orcl-1995-2014.txt')
+# 加载内置测试数据
+data = bt.feeds.GenericCSVData(
+    dataname=data_path,
+    fromdate=bt.datetime.datetime(2000, 1, 1),
+    todate=bt.datetime.datetime(2001, 1, 1),
+    dtformat=('%Y-%m-%d'),
+    openinterest=-1
+)
+cerebro.adddata(data)  # Add the data feed
+
+cerebro.addstrategy(SmaCross)  # Add the trading strategy
+cerebro.run()  # run it all
+cerebro.plot()  # and plot it with a single command
